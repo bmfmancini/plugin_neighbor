719e8950895d1506d053908838f997752f61c3a1
Sean Mancini <sean@seanmancini.com>
2026-02-16 11:30:31 -0500

    doc block updates

--- lib/polling.php
+++ lib/polling.php
@@ -1,8 +1,7 @@
 <?php
 /*
- ex: set tabstop=4 shiftwidth=4 autoindent:
  +-------------------------------------------------------------------------+
- | Copyright (C) 2006-2017 The Cacti Group                                 |
+ | Copyright (C) 2004-2026 The Cacti Group                                 |
  |                                                                         |
  | This program is free software; you can redistribute it and/or           |
  | modify it under the terms of the GNU General Public License             |
@@ -23,13 +22,19 @@
  +-------------------------------------------------------------------------+
 */
 
+/**
+ * Get the edge RRA
+ *
+ * @return array The edge RRA
+ */
 function get_edge_rra() {
-	
 	$rrd_array = [];
 	$rows = db_fetch_assoc("SELECT rrd_file from plugin_neighbor_edge");
+
 	if (!is_array($rows)) {
 		return $rrd_array;
 	}
+
 	foreach ($rows as $row) {
 		$rrd = isset($row['rrd_file']) ? $row['rrd_file'] : "";
 		if ($rrd) {
@@ -38,18 +43,25 @@ function get_edge_rra() {
 	}
 	return $rrd_array;
 }
+
+/**
+ * Process the poller output
+ *
+ * @param array $rrd_update_array The RRD update array
+ * @return array The updated RRD update array
+ */
 function neighbor_poller_output(&$rrd_update_array) {
 	global $config, $debug;
 
 	$edge_rra = get_edge_rra();
-	//db_execute_prepared("INSERT into plugin_neighbor_log values (?,NOW(),?)",array('',"Edges:".print_r($edge_rra,true)));
 	$path_rra = $config['rra_path'];
+
 	foreach ($rrd_update_array as $rrd => $rec) {
 		$rra_subst = str_replace($path_rra,"<path_rra>",$rrd);
-		//db_execute_prepared("INSERT into plugin_neighbor_log values (?,NOW(),?)",array('',"$rra_subst:".print_r($rec,true)));
+
 		if (!isset($edge_rra[$rra_subst])) {
 			continue;
-		}		// No point in storing outputs for everything
+		}
 		
 		$rec_json = json_encode($rec);
 		foreach ($rec['times'] as $time => $data) {
@@ -67,38 +79,39 @@ function neighbor_poller_output(&$rrd_update_array) {
 		
 	}
 	
-	db_execute_prepared("DELETE FROM plugin_neighbor_poller_output where timestamp < ?", array(time() - 900));	// Nothing older than 15 minutes
+	db_execute_prepared("DELETE FROM plugin_neighbor_poller_output where timestamp < ?", array(time() - 900));
+
 	return $rrd_update_array;
 }
 
-// Process the deltas from the poller_output hook
-// Called from poller_bottom hook
-
-
-
+/**
+ * Process the deltas from the poller_output hook
+ * Called from poller_bottom hook
+ */
 function process_poller_deltas() {
-	
 	cacti_log("process_poller_deltas() is running", true, "NEIGHBOR POLLER");
-	
-	// Fetch the poller output samples
 
 	db_execute_prepared("INSERT into plugin_neighbor_log values (?,NOW(),?)",array('','process_poller_deltas() is starting.'));
+
 	$results = db_fetch_assoc("SELECT * from plugin_neighbor_poller_output");
+
 	if (!is_array($results)) {
 		return;
 	}
-	//db_execute_prepared("INSERT into plugin_neighbor_log values (?,NOW(),?)",array('','process_poller_deltas() has run db_fetch_assoc'));
+
 	$hash = db_fetch_hash($results,array('rrd_file','timestamp','key_name'));
+
 	if (!is_array($hash)) {
 		return;
 	}
-	//db_execute_prepared("INSERT into plugin_neighbor_log values (?,NOW(),?)",array('','process_poller_deltas() has run db_fetch_hash'));
 
 		
 	foreach ($hash as $rrdFile => $data) {
 		cacti_log("process_poller_deltas() is processing RRD:$rrdFile,with data:" . print_r($data,1), true, "NEIGHBOR POLLER");
+
 		$timestamps = array_keys($data);
-		rsort($timestamps);		// We want the last two timestamps, so order them in reverse
+		rsort($timestamps);
+		
 		db_execute_prepared("INSERT into plugin_neighbor_log values (?,NOW(),?)",array('','process_poller_deltas() is running. Timestamps:'.print_r($timestamps,1)));
 		
 		if (sizeof($timestamps) >= 2) {
@@ -106,7 +119,10 @@ function process_poller_deltas() {
 			$before = $timestamps[1];
 			$timeDelta = $now - $before;
 			$poller_interval = read_config_option('poller_interval') ? read_config_option('poller_interval') : 300;
-			$timestamp_cycle = intval($now / $poller_interval) * $poller_interval ;	// Normalise these down to a poller cycle boundary to group them together
+			
+			/* Normalise these down to a poller cycle boundary to group them together */
+			$timestamp_cycle = intval($now / $poller_interval) * $poller_interval ;
+			
 			cacti_log("process_poller_deltas(): now:$now, before:$before, Hash:".print_r($data[$now],true), true, "NEIGHBOR POLLER");
 			db_execute_prepared("INSERT into plugin_neighbor_log values (?,NOW(),?)",array('',"Now:$now, Before:$before, Hash:".print_r($data[$now],true)));
 			foreach ($data[$now] as $key => $record) {
@@ -119,34 +135,51 @@ function process_poller_deltas() {
 			}
 		}
 	}
-	db_execute_prepared("DELETE FROM plugin_neighbor_poller_delta where timestamp < ?", array(time() - 900));	// Nothing older than 15 minutes
+	
+	/* Nothing older than 15 minutes */
+	db_execute_prepared("DELETE FROM plugin_neighbor_poller_delta where timestamp < ?", array(time() - 900));
 }
 
 
+/**
+ * Fetch a hash from a DB result
+ *
+ * @param array $result The DB result
+ * @param array $index_keys The index keys
+ * @return array The hash
+ */
 function db_fetch_hash(& $result,$index_keys) {
-  $assoc = array();             // The array we're going to be returning
-  foreach ($result as $row) {
+	/* The array we're going to be returning */
+	$assoc = array(); 
+	
+	foreach ($result as $row) {
+		/* Start the pointer off at the base of the array */
+		$pointer = & $assoc;
+		
+		for ($i=0; $i<count($index_keys); $i++) {
+			$key_name = $index_keys[$i];
+			if (!array_key_exists($key_name,$row)) {
+				error_log("Error: Key [$key_name] is not present in the results output\n");
+				return(false);
+			}
 
-        $pointer = & $assoc;            // Start the pointer off at the base of the array
-        for ($i=0; $i<count($index_keys); $i++) {
-                $key_name = $index_keys[$i];
-								if (!array_key_exists($key_name,$row)) {
-                        error_log("Error: Key [$key_name] is not present in the results output\n");
-                        return(false);
-                }
+			$key_val= isset($row[$key_name]) ? $row[$key_name]  : "";
+			
+			if (!isset($pointer[$key_val])) {
+				/* Start a new node */
+				$pointer[$key_val] = "";
+				
+				/* Move the pointer on to the new node */
+				$pointer = & $pointer[$key_val];
+			}
+			else {
+				/* Already exists, move the pointer on to the new node */
+				$pointer = & $pointer[$key_val];
+			}
+		}
 
-                $key_val= isset($row[$key_name]) ? $row[$key_name]  : "";
-                if (!isset($pointer[$key_val])) {
-
-                        $pointer[$key_val] = "";                // Start a new node
-                        $pointer = & $pointer[$key_val];                // Move the pointer on to the new node
-                }
-                else {
-                        $pointer = & $pointer[$key_val];            // Already exists, move the pointer on to the new node
-                }
-        } // for $i
-        foreach ($row as $key => $val) { $pointer[$key] = $val; }
-  } // $row
-  return($assoc);
+		foreach ($row as $key => $val) { $pointer[$key] = $val; }
+	}
+	return($assoc);
 }
 
